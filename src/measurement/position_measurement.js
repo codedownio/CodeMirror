bb95ffbdfa054b68507a4f17cc8cb16f55b7d156
Tom McLaughlin <pyro777@gmail.com>
2018-02-02 01:09:51 -0800

    Add size helper to facilitate centering

--- src/measurement/position_measurement.js
+++ src/measurement/position_measurement.js
@@ -621,7 +621,9 @@ export function getDimensions(cm) {
     left[cm.options.gutters[i]] = n.offsetLeft + n.clientLeft + gutterLeft
     width[cm.options.gutters[i]] = n.clientWidth
   }
-  return {fixedPos: compensateForHScroll(d),
+  // Hack: I don't care about H scrolling, so I changed fixedPos to
+  // facilitate the centering trick with .CodeMirror-size-helper
+  return {fixedPos: -parseInt(d.sizer.style.marginLeft),
           gutterTotalWidth: d.gutters.offsetWidth,
           gutterLeft: left,
           gutterWidth: width,
