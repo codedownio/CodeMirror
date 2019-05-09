87e58d981328e552db1821f96a8f7e9958c8ce32
Tom McLaughlin <pyro777@gmail.com>
2019-05-09 04:38:20 -0700

    Make selection draw better in the presence of padding/margin on the sizeHelper

--- src/display/selection.js
+++ src/display/selection.js
@@ -53,7 +53,8 @@ function drawSelectionRange(cm, range, output) {
   let display = cm.display, doc = cm.doc
   let fragment = document.createDocumentFragment()
   let padding = paddingH(cm.display), leftSide = padding.left
-  let rightSide = Math.max(display.sizerWidth, displayWidth(cm) - (display.sizeHelper.offsetLeft + display.sizer.offsetLeft)) - padding.right
+
+  let rightSide = display.lineDiv.offsetWidth - padding.right;
   let docLTR = doc.direction == "ltr"
 
   function add(left, top, width, bottom) {
